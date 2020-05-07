5ac45df17864322854e8534502fd2a06c6355c87
Sir_Mount <aron@mayen.at>
2020-05-07 13:50:51 +0200

    Functionele Header

--- EenmaalAndermaal-Groep-33/includes/header.php
+++ EenmaalAndermaal-Groep-33/includes/header.php
@@ -1,19 +1,27 @@
-<nav class="navbar navbar-fixed-top">
+<nav class="navbar navbar-inverse navbar-fixed-top">
   <div class="container-fluid">
     <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="index.php"> <img src="Afbeeldingen/logo.png" alt="Logo" style="height:100%;"/>
+		<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+		</button>
+		<a class="navbar-brand" href="index.php"> <img src="Afbeeldingen/logo.png" alt="Logo" style="height:100%;"/></a>
     </div>
-    <div class="collapse navbar-collapse" id="myNavbar">
+    <div class="navbar-collapse" id="myNavbar">
       <ul class="nav navbar-nav">
-        <li><a href="index.php" >Home</a> </li>
-        <li><a href="productlist.php">Producten</a></li>
-        <li><a href="#">Over</a></li>
-        <li><a href="#">Contact</a></li>
+		<?php
+			$calling_file = basename($_SERVER['REQUEST_URI']);
+			if($calling_file == "index.php") {echo '<li class = "active"><a href="index.php">Home</a> </li>';} else{
+				echo '<li><a href="index.php">Home</a> </li>';
+			}
+			if($calling_file == "productlist.php") {echo '<li class = "active"><a href="productlist.php">Producten</a></li>';} else{
+				echo '<li><a href="productlist.php">Producten</a></li>';
+			}
+			if($calling_file == "contact.php") {echo '<li class = "active"><a href="contact.php">Contact</a></li>';} else{
+				echo '<li><a href="contact.php">Contact</a></li>';
+			}
+		?>
       </ul>
       <form class="navbar-form navbar-left" action="#">
         <div class="form-group">
