947e6c563360120847b48a6abfa4c7e9ec0359f8
BorisOtte <boris-otte@hotmail.nl>
2020-05-14 21:31:31 +0200

    Toevoegen Form en eerste versie account informatie wijziging

--- EenmaalAndermaal-Groep-33/account.php
+++ EenmaalAndermaal-Groep-33/account.php
@@ -24,6 +24,7 @@ if(!isset($_SESSION['userName']))
 </header>
 <body>
 <br>
+  <?php echo($alteredAccountInformationNotification) ?>
   <div class="container-fluid text-left">
     <div class="row content">
       <div class="col-sm-2">
@@ -32,8 +33,71 @@ if(!isset($_SESSION['userName']))
       <div class="col-sm-2">
         <h1> Account informatie </h1>
         <h2> <?php echo $_SESSION['userName'] ?> </h2>
-        <p> <?php echo $informationHTML ?> </p>
-        <a href="#" class="btn btn-primary">Wijzig account informatie</a>
+        <form class="form-horizontal" role="form" action="account.php" method="post">
+            <div class="form-group">
+                <label for="Emailadres" class="control-label">Emailadres</label>
+                <input type="email" maxlength="50" name="mailBox" id="Emailadres" placeholder="Emailadres" value="<?php echo(htmlspecialchars($accountInformation['email'], ENT_QUOTES)) ?>" class="form-control">
+                <?php echo($mailBoxErrorMessage) ?>
+            </div>
+            <div class="form-group">
+                <label for="Voornaam" class="control-label">Voornaam</label>
+                <input type="text" maxlength="50"name="name" id="Voornaam" placeholder="Voornaam" value="<?php echo(htmlspecialchars($accountInformation['voornaam'], ENT_QUOTES)) ?>" class="form-control">
+                <?php echo($nameErrorMessage) ?>
+            </div>
+            <div class="form-group">
+                <label for="Achternaam" class="control-label">Achternaam</label>
+                <input type="text" maxlength="58" name="surname" id="Achternaam" placeholder="Achternaam" value="<?php echo(htmlspecialchars($accountInformation['achternaam'], ENT_QUOTES)) ?>" class="form-control">
+                <?php echo($surnameErrorMessage) ?>
+            </div>
+            <div class="form-group">
+                <label for="Adresregel1" class="control-label">Adresregel1</label>
+                <input type="text" maxlength="55" name="address1" id="Adresregel1" placeholder="Adresregel" value="<?php echo(htmlspecialchars($accountInformation['adresregel1'], ENT_QUOTES)) ?>" class="form-control">
+                <?php echo($address1ErrorMessage) ?>
+            </div>
+            <div class="form-group">
+                <label for="Adresregel2" class="control-label">Adresregel2 (optioneel)</label>
+                <input type="text" maxlength="55" name="address2" id="Adresregel2" placeholder="Adresregel" value="<?php echo(htmlspecialchars($accountInformation['adresregel2'], ENT_QUOTES)) ?>" class="form-control">
+                <?php echo($address2ErrorMessage) ?>
+            </div>
+            <div class="form-group">
+                <label for="Postcode" class="control-label">Postcode</label>
+                <input type="text" size="6" maxlength="6" name="postCode" id="Postcode" placeholder="Postcode" value="<?php echo(htmlspecialchars($accountInformation['postcode'], ENT_QUOTES)) ?>" class="form-control">
+                <?php echo($postCodeErrorMessage) ?>
+            </div>
+            <div class="form-group">
+                <label for="plaatsnaam" class="control-label">Plaatsnaam</label>
+                <input type="text" maxlength="28" name="placeName" id="Plaatsnaam" placeholder="Plaatsnaam" value="<?php echo(htmlspecialchars($accountInformation['plaatsnaam'], ENT_QUOTES)) ?>" class="form-control">
+                <?php echo($placeNameErrorMessage) ?>
+            </div>
+            <div class="form-group">
+                <label for="Land" class="control-label">Land</label>
+                    <select id="country" name="country" value="<?php echo(htmlspecialchars($accountInformation['land'], ENT_QUOTES)) ?>" class="form-control">
+                        <option value="Nederland">Nederland</option>
+                        <option value="België">België</option>
+                        <option value="Denemarken">Denemarken</option>
+                        <option value="Duitsland">Duitsland</option>
+                        <option value="Italië">Italië</option>
+                        <option value="Spanje">Spanje</option>
+                        <option value="Zweden">Zweden</option>
+                    </select>
+            </div> <!-- /.form-group -->
+            <div class="form-group">
+                <label for="Geboortedatum" class="control-label">Geboortedatum</label>
+                <input type="date" name="birthDate" id="Geboortedatum" value="<?php echo date_format($accountInformation['geboorteDag'], 'Y-m-d') ?>" class="form-control">
+                <?php echo($birthDateErrorMessage) ?>
+            </div>
+
+            <!-- PLACEHOLDER VOOR BESTAANDE TELEFOONNUMMERS -->
+
+            <div class="form-group">
+                <label for="Telefoonnummer" class="control-label">Nieuw Telefoonnummer</label>
+                <input type="text" maxlength="15"name="telephoneNumber" id="Telefoonnummer" placeholder="Telefoonnummer" class="form-control">
+                <?php echo($nameErrorMessage) ?>
+            </div>
+            <div class="form-group">
+                    <button type="submit" class="btn btn-primary btn-block" name="accountInformationButton">Wijzig account informatie</button>
+            </div>
+        </form> <!-- /form -->
       </div>
 
       <div class="col-sm-3">
