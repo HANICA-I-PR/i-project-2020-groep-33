f3ac924ca3a5f44e28b5a6b8769df4b91d54f940
Sir_Mount <aron@mayen.at>
2020-05-15 01:06:04 +0200

    Rubriek pagina aangemaakt

---
+++ EenmaalAndermaal-Groep-33/rubriekBeheer.php
@@ -0,0 +1,82 @@
+<!DOCTYPE php>
+<?php
+	include('includes/connect.php');
+	include('includes/itemToCard.php');
+?>
+
+<html lang="en">
+	<head>
+		<title>EenmaalAndermaal</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+		<link rel="stylesheet" type="text/css" href="CSS/stylesheet.css">
+
+	</head>
+	<header>
+		<?php include 'includes/header.php' ?>
+	</header>
+	<body>
+		<div class="container">
+<!-- Page Heading -->
+			<h1 class="my-4">Rubrieken</h1><br>
+
+			<div class="row">
+				<?php
+					$tsql = "SELECT rubrieknaam, rubrieknummer FROM tbl_Rubriek WHERE rubriek IS NULL ORDER BY volgnr ASC, rubrieknaam ASC";
+					$query = sqlsrv_query($conn, $tsql, NULL);
+
+					if ( $query === false) {
+						die( FormatErrors( sqlsrv_errors() ) );
+					} else {
+						$categorieen = '';
+	//hieronder in while loop wordt de select van $tsql query gefetcht
+						while ($row = sqlsrv_fetch_array( $query, SQLSRV_FETCH_ASSOC)) {
+	//Hieronder worden de rubrieknamen en nummers van de subrubrieken geselecteerd en sorteert eerst op volgnr daarna op rubrieknaam
+							$sub_tsql = "SELECT rubrieknaam, rubrieknummer FROM tbl_Rubriek WHERE rubriek = $row[rubrieknummer] ORDER BY volgnr ASC, rubrieknaam ASC";
+							$sub_query = sqlsrv_query($conn, $sub_tsql, NULL);
+
+							$categorieen .= "<div class='well well-sm'>";
+								$categorieen .= "<h3>".$row['rubrieknaam']."    ";
+									$categorieen .= "<button type='button' class='btn btn-link' data-toggle='collapse' data-target=#".$row['rubrieknummer'].">SubRubrieken</button>";
+								$categorieen .= "</h3>";
+
+								$categorieen .= "<div class='text-right'>";
+									$categorieen .= "<div class='btn-group '>";
+										$categorieen .= "<button type='button' class='btn btn-primary' action='#'>Nieuw</button>";
+										$categorieen .= "<button type='button' class='btn btn-warning' action='#'>Hernoem</button>";
+										$categorieen .= "<button type='button' class='btn btn-danger' action='#'>Verwijder</button>";
+									$categorieen .= "</div>";
+								$categorieen .= "</div>";
+
+
+								$categorieen .= "<div id=".$row['rubrieknummer']." class='collapse'>";
+									$categorieen .= "<ul style='list-style-type: none'>";
+										while($sub_row = sqlsrv_fetch_array( $sub_query, SQLSRV_FETCH_ASSOC))  {
+											$categorieen .= "<li>".$sub_row['rubrieknaam']."";
+
+												$categorieen .= "<div class='text-right'>";
+													$categorieen .= "<div class='btn-group '>";
+														$categorieen .= "<button type='button' class='btn btn-warning' action='#'>Hernoem</button>";
+														$categorieen .= "<button type='button' class='btn btn-danger' action='#'>Verwijder</button>";
+													$categorieen .= "</div>";
+												$categorieen .= "</div>";
+											$categorieen .= "</li>";
+										}
+									$categorieen .= "</ul>";
+								$categorieen .= "</div>";
+							$categorieen .= "</div>";
+						}
+
+						echo $categorieen;
+					}
+				?>
+			</div>
+		</div>
+	</body>
+	<footer class="container-fluid text-center">
+		<?php include 'includes/footer.php' ?>
+	</footer>
+</html>
