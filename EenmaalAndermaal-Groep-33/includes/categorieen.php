12c661175554ab4cd598f586835da818b0756ce1
Sir_Mount <aron@mayen.at>
2020-05-14 16:06:27 +0200

    relative header

--- EenmaalAndermaal-Groep-33/includes/categorieen.php
+++ EenmaalAndermaal-Groep-33/includes/categorieen.php
@@ -1,49 +1,43 @@
-<div id="navbarCollapse" class="collapse navbar-collapse">
+	<div id="navbarCollapse" class="collapse navbar-collapse">
 
-	<?php
-	  /* Onderstaande query selecteert rubrieknummer en naam van de main categorieen en sorteert eerst
-		   op volgnr daarna op rubrieknaam */
-		$tsql = "SELECT rubrieknaam, rubrieknummer FROM tbl_Rubriek WHERE rubriek IS NULL ORDER BY volgnr ASC, rubrieknaam ASC";
-	  $query = sqlsrv_query($conn, $tsql, NULL);
-	  if ( $query === false)
-		{
-			die( FormatErrors( sqlsrv_errors() ) );
-		} else {
-	     $categorieen = '';
-			 /* hieronder in while loop wordt de select van $tsql query gefetcht*/
-	     while ($row = sqlsrv_fetch_array( $query, SQLSRV_FETCH_ASSOC)) {
-			 /* hieronder worden de rubrieknamen en nummers van de subrubrieken geselecteerd en sorteert eerst
-		 	   op volgnr daarna op rubrieknaam*/
-	     $tsql1 = "SELECT rubrieknaam, rubrieknummer FROM tbl_Rubriek WHERE rubriek = $row[rubrieknummer] ORDER BY volgnr ASC, rubrieknaam ASC";
-	     $query1 = sqlsrv_query($conn, $tsql1, NULL);
+		<?php
+		  /* Onderstaande query selecteert rubrieknummer en naam van de main categorieen en sorteert eerst
+			   op volgnr daarna op rubrieknaam */
+			$tsql = "SELECT rubrieknaam, rubrieknummer FROM tbl_Rubriek WHERE rubriek IS NULL ORDER BY volgnr ASC, rubrieknaam ASC";
+		  $query = sqlsrv_query($conn, $tsql, NULL);
+		  if ( $query === false)
+			{
+				die( FormatErrors( sqlsrv_errors() ) );
+			} else {
+		     $categorieen = '';
+				 /* hieronder in while loop wordt de select van $tsql query gefetcht*/
+		     while ($row = sqlsrv_fetch_array( $query, SQLSRV_FETCH_ASSOC)) {
+				 /* hieronder worden de rubrieknamen en nummers van de subrubrieken geselecteerd en sorteert eerst
+			 	   op volgnr daarna op rubrieknaam*/
+		     $tsql1 = "SELECT rubrieknaam, rubrieknummer FROM tbl_Rubriek WHERE rubriek = $row[rubrieknummer] ORDER BY volgnr ASC, rubrieknaam ASC";
+		     $query1 = sqlsrv_query($conn, $tsql1, NULL);
 
-	    $categorieen .= "<div class='dropdown'>";
-	    $categorieen .= " <a class='btn btn-secondary dropdown-toggle'href='#'role='button'id='dropdownMenuLink' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>";
-	    /* $row['rubrieknaam']: rubrieknaam van de main categorieen*/
-		  $categorieen .= $row['rubrieknaam']. "</a>";
-	    $categorieen .= "<div class='dropdown-menu' aria-labelledby='dropdownMenuLink'>";
-	    $categorieen .= "<ul id = 'ul_dropdown'>";
-			/* hieronder in de while loop wordt de select query tsql1 van de subrubrieken gefetcht*/
-			while($row1 = sqlsrv_fetch_array( $query1, SQLSRV_FETCH_ASSOC))  {
-				/* $row1['rubrieknummer'] wordt in de link meegegeven om in productlist.php te zoeken*/
-	      $categorieen .= "<li> <a class='dropdown-item' href='productlist.php?rubriek=".$row1['rubrieknummer']."'>";
-				/* $row1['rubrieknaam']: naam van subrubriek*/
-	      $categorieen .= $row1['rubrieknaam']." </a> </li>";
-	    }
-	    $categorieen .= " </ul>";
-	    $categorieen .=  "</div>";
-	    $categorieen .=  " </div>";
+		    $categorieen .= "<div class='dropdown'>";
+		    $categorieen .= " <a class='btn btn-secondary dropdown-toggle'href='#'role='button'id='dropdownMenuLink' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>";
+		    /* $row['rubrieknaam']: rubrieknaam van de main categorieen*/
+			  $categorieen .= $row['rubrieknaam']. "</a>";
+		    $categorieen .= "<div class='dropdown-menu' aria-labelledby='dropdownMenuLink'>";
+		    $categorieen .= "<ul id = 'ul_dropdown'>";
+				/* hieronder in de while loop wordt de select query tsql1 van de subrubrieken gefetcht*/
+				while($row1 = sqlsrv_fetch_array( $query1, SQLSRV_FETCH_ASSOC))  {
+					/* $row1['rubrieknummer'] wordt in de link meegegeven om in productlist.php te zoeken*/
+		      $categorieen .= "<li> <a class='dropdown-item' href='productlist.php?rubriek=".$row1['rubrieknummer']."'>";
+					/* $row1['rubrieknaam']: naam van subrubriek*/
+		      $categorieen .= $row1['rubrieknaam']." </a> </li>";
+		    }
+		    $categorieen .= " </ul>";
+		    $categorieen .=  "</div>";
+		    $categorieen .=  " </div>";
 
-	        }
-	        echo $categorieen;
-	  }
-	?>
-  </div>
-
-
-</nav>
-<br>
-<br>
-<br>
-<br>
-<br>
+		        }
+		        echo $categorieen;
+		  }
+		?>
+	  </div>
+	</nav>
+</section>
--- EenmaalAndermaal-Groep-33/includes/header.php
+++ EenmaalAndermaal-Groep-33/includes/header.php
@@ -1,46 +1,47 @@
-<nav class="navbar navbar-inverse navbar-fixed-top">
-  <div class="container-fluid">
-    <div class="navbar-header">
-		<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-			<span class="icon-bar"></span>
-			<span class="icon-bar"></span>
-			<span class="icon-bar"></span>
-		</button>
-		<a class="navbar-brand" href="index.php"> <img src="Afbeeldingen/logo.png" alt="Logo" style="height:100%;"/></a>
-    </div>
-    <div id="myNavbar" class="collapse navbar-collapse">
-      <ul class="nav navbar-nav">
-		<?php
-			$calling_file = basename($_SERVER['REQUEST_URI']);
-			if($calling_file == "index.php") {echo '<li class = "active"><a href="index.php">Home</a> </li>';} else{
-				echo '<li><a href="index.php">Home</a> </li>';
-			}
-			if($calling_file == "productlist.php") {echo '<li class = "active"><a href="productlist.php">Producten</a></li>';} else{
-				echo '<li><a href="productlist.php">Producten</a></li>';
-			}
-			if($calling_file == "contact.php") {echo '<li class = "active"><a href="contact.php">Contact</a></li>';} else{
-				echo '<li><a href="contact.php">Contact</a></li>';
-			}
-		?>
-      </ul>
-      <form class="navbar-form navbar-left" action="productlist.php" method="post">
-        <div class="form-group">
-          <input type="text"  name= "term" class="form-control" placeholder="Waar ben je naar op zoek">
-        </div>
-        <button type="submit" name="zoeken" class="btn btn-default">Zoek</button>
-      </form>
+<section id="header">
+	<nav class="navbar navbar-inverse navbar-fixed-top">
+	  <div class="container-fluid">
+	    <div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="index.php"> <img src="Afbeeldingen/logo.png" alt="Logo" style="height:100%;"/></a>
+	    </div>
+	    <div id="myNavbar" class="collapse navbar-collapse">
+	      <ul class="nav navbar-nav">
+			<?php
+				$calling_file = basename($_SERVER['REQUEST_URI']);
+				if($calling_file == "index.php") {echo '<li class = "active"><a href="index.php">Home</a> </li>';} else{
+					echo '<li><a href="index.php">Home</a> </li>';
+				}
+				if($calling_file == "productlist.php") {echo '<li class = "active"><a href="productlist.php">Producten</a></li>';} else{
+					echo '<li><a href="productlist.php">Producten</a></li>';
+				}
+				if($calling_file == "contact.php") {echo '<li class = "active"><a href="contact.php">Contact</a></li>';} else{
+					echo '<li><a href="contact.php">Contact</a></li>';
+				}
+			?>
+	      </ul>
+	      <form class="navbar-form navbar-left" action="productlist.php" method="post">
+	        <div class="form-group">
+	          <input type="text"  name= "term" class="form-control" placeholder="Waar ben je naar op zoek">
+	        </div>
+	        <button type="submit" name="zoeken" class="btn btn-default">Zoek</button>
+	      </form>
 
-      <ul class="nav navbar-nav navbar-right">
-        <?php if(isset($_SESSION['userName']))
-              {
-                echo("<li><a href='account.php'><span class='glyphicon glyphicon-user'></span> Account</a></li>");
-                echo("<li><a href='logout.php'><span class='glyphicon glyphicon-log-out'></span> Logout</a></li>");
-              }
-              else
-              {
-                echo("<li><a href='login.php'><span class='glyphicon glyphicon-log-in'></span> Login</a></li>");
-              }?>
-      </ul>
-    </div>
-  </div>
+	      <ul class="nav navbar-nav navbar-right">
+	        <?php if(isset($_SESSION['userName']))
+	              {
+	                echo("<li><a href='account.php'><span class='glyphicon glyphicon-user'></span> Account</a></li>");
+	                echo("<li><a href='logout.php'><span class='glyphicon glyphicon-log-out'></span> Logout</a></li>");
+	              }
+	              else
+	              {
+	                echo("<li><a href='login.php'><span class='glyphicon glyphicon-log-in'></span> Login</a></li>");
+	              }?>
+	      </ul>
+	    </div>
+	  </div>
 <?php include "categorieen.php"; ?>
