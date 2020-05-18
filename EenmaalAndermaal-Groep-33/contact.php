09cdeac6c4cfc2c1a5a04494598885730c7ea03d
BorisOtte <boris-otte@hotmail.nl>
2020-05-18 11:58:02 +0200

    contact pagina tekst update

--- EenmaalAndermaal-Groep-33/contact.php
+++ EenmaalAndermaal-Groep-33/contact.php
@@ -22,16 +22,16 @@ include('includes/connect.php')
   <body>
     <section id="contact">
        <div class="container">
-           <h3 class="text-center text-uppercase">contact ons</h3>
-           <p class="text-center w-75 m-auto">Heb je een vraag over de werking van de website? In het Call Center hebben we alle handleidingen voor je klaargezet. Komen we er alsnog niet uit? Dan kun je in de mail een vraag aan ons stellen. Jouw vraag komt direct bij de juiste medewerker zodat je snel antwoord hebt op je vraag</p>
+           <h3 class="text-center text-uppercase">Neem contact met ons op</h3>
+           <p class="text-center w-75 m-auto">Hebt u een vraag over de werking van de website? In het Call Center hebben we alle handleidingen voor u klaargezet. Komen we er alsnog niet uit? Dan kunt u in de mail een vraag aan ons stellen. Uw vraag komt direct bij de juiste medewerker zodat u snel antwoord hebt op uw vraag</p>
            <div class="row">
              <div class="col-sm-2"></div>
              <div class="col-sm-3">
                <div class="card border-0">
                   <div class="card-body text-center">
                     <i class="fa fa-phone fa-5x mb-3" aria-hidden="true"></i>
-                    <h4 class="text-uppercase mb-5">bel ons</h4>
-                    <p>call/whatsapp +33 024-3530500 <br> Bereikbaar op werkdagen van 09:00 tot 16:30 uur</p>
+                    <h4 class="text-uppercase mb-5">Bel ons</h4>
+                    <p>telefoon/whatsapp +33 024-3530500 <br> Bereikbaar op werkdagen van 09:00 tot 16:30 uur</p>
                   </div>
                 </div>
              </div>
