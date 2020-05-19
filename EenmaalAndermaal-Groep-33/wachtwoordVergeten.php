c8107f73875241e15ee311822035583f7990ed35
BorisOtte <boris-otte@hotmail.nl>
2020-05-19 11:56:45 +0200

    je vervangen met u, invoerveld iets aangepast

--- EenmaalAndermaal-Groep-33/wachtwoordVergeten.php
+++ EenmaalAndermaal-Groep-33/wachtwoordVergeten.php
@@ -13,13 +13,11 @@ include('includes/header.php');
                 <div class="text-center">
                   <h3><i class="fa fa-lock fa-4x"></i></h3>
                   <h2 class="text-center">Wachtwoord vergeten</h2>
-                  <p>Weet je het wachtwoord niet meer? Vul hieronder je e-mailadres in. We sturen dan binnen enkele minuten een e-mail waarmee een nieuw wachtwoord kan worden aangemaakt.</p>
+                  <p>Weet u uw wachtwoord niet meer? Vul hieronder uw e-mailadres in. We sturen dan binnen enkele minuten een e-mail waarmee een nieuw wachtwoord kan worden ingesteld.</p>
                   <div class="panel-body">
                     <form id="register-form" role="form" autocomplete="off" class="form" method="post">
                       <div class="form-group">
-                        <div class="input-group">
                           <input type="text" name="emailadres" id="email" placeholder="Uw e-mailadres" class="form-control type="email"">
-                        </div>
                       </div>
                       <div class="form-group">
                         <input name="recover-submit" class="btn btn-lg btn-primary btn-block" value="Verzenden" type="submit">
